cc8640a14e0f1e3c57cbf5a7d919aa5dc7a46d08
m4ddav3 <m4ddav3@users.noreply.github.com>
2015-04-14 17:36:45 +0100

    Playing with different colouring

--- mandy.pl
+++ mandy.pl
@@ -3,16 +3,26 @@ use strict; use warnings;
 
 use Data::Dumper;
 use GD;
+#use bignum;
 
-my $scale = 1/300;
-#my ($y_size, $x_size) = (2000, 3000);
+my $scale = (1/51200);
+my ($y_size, $x_size) = (900, 1600);
 #my ($y_size, $x_size) = (60, 90);
-my ($y_size, $x_size) = (90, 160);
-my ($x_origin, $y_origin) = (0.25, 0);
-#my ($x_origin, $y_origin) = (-0.7453,0.1127);
+#my ($y_size, $x_size) = (90, 160);
+#my ($x_origin, $y_origin) = (-1.75005, -0.011802);
+#my ($x_origin, $y_origin) = (-1.75004622, -0.011802019);
+my ($x_origin, $y_origin) = (-0.7453,0.1127);
 
-my $max_iters = 65535;#(65535) - 1;
+my $max_iters   = 16777215;
+my $max_colours = 16777215; # 255 (b), 65536 (gb), or 16777215 (rgb)
+
 my $limit     = 4;
+
+my $output_filename = sprintf('mandy-%d.png', time);
+
+my $pixels = [[]];
+my $low_pixel  = $max_iters;
+my $high_pixel = 0;
 
 sub val_to_rgb {
     my ($val) = @_;
@@ -23,7 +33,7 @@ sub val_to_rgb {
     $val = $val >> 8;
     my $r = $val & 0xff;
 
-    print sprintf("%s -> (%s, %s, %s)\n", $_[0], $r, $g, $b);
+    print sprintf("%s -> (%3s, %3s, %3s)\n", $_[0], $r, $g, $b);
 
     return ($r, $g, $b);
 }
@@ -31,8 +41,8 @@ sub val_to_rgb {
 sub scale_int {
     my ($val, $in_min, $in_max, $out_min, $out_max) = @_;
 
-    return ($val - $in_min) * ($out_max - $out_min)
-           / ($in_max - $in_min) + $out_min;
+    return int(($val - $in_min) * ($out_max - $out_min)
+           / ($in_max - $in_min) + $out_min + 0.5);
 }
 
 print sprintf("Scale is: %s\n", $scale);
@@ -58,7 +68,7 @@ print sprintf("Y: %s to %s = %s to %s\n", 0, $y_size, ($min_y + 0) * $scale, ($m
 my $image = GD::Image->new($x_size, $y_size, 1);
 
 my $colours = {
-    0        => $image->colorAllocate(0,0,0),
+    0        => $image->colorAllocate(  0,  0,  0),
     16777215 => $image->colorAllocate(255,255,255),
 };
 
@@ -112,25 +122,34 @@ for my $y (@yvals) {
         if ($iters > $max_iters) {
             # Convergence is black (palette entry #0)
             $image->setPixel($x, $y, 0);
+            $pixels->[$x]->[$y] = 0;
         }
         else {
-            #my ($r, $g, $b) = val_to_rgb(scale_int($iters, 0, $max_iters, 0, 16777215));
-            if ($max_iters > 16777215) {
-                $iters = scale_int($iters, 0, $max_iters, 0, 16777215);
+            $pixels->[$x]->[$y] = $iters;
+            if ($iters < $low_pixel) {
+                $low_pixel = $iters;
             }
+            elsif ($iters > $high_pixel) {
+                $high_pixel = $iters;
+            }
+
+            #my ($r, $g, $b) = val_to_rgb(scale_int($iters, 0, $max_iters, 0, 16777215));
+            #if ($max_iters > 16777215) {
+                $iters = scale_int($iters, 0, $max_iters, 0, $max_colours);
+            #}
             #$colours->{$iters} //= $image->colorAllocate(val_to_rgb(scale_int($iters, 0, $max_iters, 0, 16777215)));
             $colours->{$iters} //= $image->colorAllocate(val_to_rgb($iters));
             $image->setPixel($x, $y, $colours->{$iters});
+
         }
 
-    }
-
-    if (time - $last_time > 10) {
-        $last_time = time;
-        open (my $fh, '>', 'mandy3.png');
-        binmode($fh);
-        print $fh $image->png;
-        close $fh;
+        if (time - $last_time > 60) {
+            $last_time = time;
+            open (my $fh, '>', $output_filename);
+            binmode($fh);
+            print $fh $image->png;
+            close $fh;
+        }
     }
 }
 
@@ -144,8 +163,26 @@ for my $p (-10..10) {
 }
 
 print Dumper $colours;
-open (my $fh, '>', 'mandy3.png');
-binmode($fh);
-print $fh $image->png;
-close $fh;
+{
+    open (my $fh, '>', $output_filename);
+    binmode($fh);
+    print $fh $image->png;
+    close $fh;
+}
 
+my $image2 = GD::Image->new($x_size, $y_size, 1);
+$colours = {};
+
+for my $x (0..$x_size) {
+    for my $y (0..$y_size) {
+        my $v = $pixels->[$x]->[$y];
+        $colours->{$v} //= $image2->colorAllocate(val_to_rgb(scale_int($v, $low_pixel, $max_colours, 0, $max_colours)));
+        $image2->setPixel($x, $y, $colours->{$v});
+    }
+}
+
+$output_filename =~ s{(\.png)}{-2$1};
+open (my $fh, '>', $output_filename);
+binmode($fh);
+print $fh $image2->png;
+close $fh;
